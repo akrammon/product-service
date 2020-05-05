7f304ef35281dc831a5a73af41e476d8961b41fa
Akos Horvath <akos.hrv@gmail.com>
2020-05-05 10:25:04 +0200

    Add javadoc to Service interface

--- src/main/java/com/akoshrv/productservice/service/ProductService.java
+++ src/main/java/com/akoshrv/productservice/service/ProductService.java
@@ -6,9 +6,26 @@ import java.util.List;
 
 public interface ProductService {
 
+    /**
+     * Find all products.
+     *
+     * @return A list of all products
+     */
     List<Product> findAllProducts();
 
+    /**
+     * Creates a new product in the system with a generated ID.
+     *
+     * @param product
+     * @return
+     */
     Product createProduct(Product product);
 
+    /**
+     * Updates the product with the supplied Product ID.
+     * 
+     * @param product
+     * @return
+     */
     Product updateProduct(Product product);
 }
