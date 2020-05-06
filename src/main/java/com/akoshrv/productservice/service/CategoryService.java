9780a30d21c3646a756b2ee7432dc03dbe795284
Akos Horvath <akos.hrv@gmail.com>
2020-05-06 11:26:10 +0200

    Add javadoc to CategoryService

--- src/main/java/com/akoshrv/productservice/service/CategoryService.java
+++ src/main/java/com/akoshrv/productservice/service/CategoryService.java
@@ -4,7 +4,19 @@ import java.util.List;
 
 public interface CategoryService {
 
+    /**
+     * Gets all the valid product categories in the system.
+     * 
+     * @return
+     */
     List<String> getAllCategories();
 
+    /**
+     * Determines if a category matches any of the categories in the system, ignoring case.
+     *
+     * @param category
+     * @return
+     */
     boolean isValidCategory(String category);
+
 }
