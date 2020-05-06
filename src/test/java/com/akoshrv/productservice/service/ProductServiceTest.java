50fc450ffc84698b37cf9a00afffb51695d8de4a
Akos Horvath <akos.hrv@gmail.com>
2020-05-06 11:52:00 +0200

    Add tests for Product filtering

--- src/test/java/com/akoshrv/productservice/service/ProductServiceTest.java
+++ src/test/java/com/akoshrv/productservice/service/ProductServiceTest.java
@@ -2,7 +2,7 @@ package com.akoshrv.productservice.service;
 
 import com.akoshrv.productservice.model.Product;
 import com.akoshrv.productservice.repository.ProductRepository;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,11 +10,16 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
+
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
 
     private static final Long TEST_ID = 1L;
-    private static final Product OLD_PRODUCT = new Product(TEST_ID, "book", 10.0, "old name", "old description");
+    private static final Product PRODUCT_1 = new Product(TEST_ID, "book", 10.0, "old name", "old description");
+    private static final Product PRODUCT_2 = new Product(2L, "movie", 12.0, "Movie title", "Description of movie");
+    private static final Product PRODUCT_3 = new Product(3L, "book", 13.0, "Title of second book", "Description of second book");
 
     @Mock
     private ProductRepository productRepository;
@@ -29,10 +34,70 @@ class ProductServiceTest {
     }
 
     @Test
+    public void findAllShouldNotFilterOutAnythingIfParamsAreNull() {
+        givenThatAllProductsAre(PRODUCT_1, PRODUCT_2, PRODUCT_3);
+
+        List<Product> result = productService.findAllProducts(null, null, null);
+
+        Assertions.assertThat(result)
+                .containsOnly(PRODUCT_1, PRODUCT_2, PRODUCT_3);
+    }
+
+    @Test
+    public void findAllShouldFilterCorrectlyBasedOnCategory() {
+        givenThatAllProductsAre(PRODUCT_1, PRODUCT_2, PRODUCT_3);
+
+        List<Product> result = productService.findAllProducts("book", null, null);
+
+        Assertions.assertThat(result)
+                .containsOnly(PRODUCT_1, PRODUCT_3);
+    }
+
+    @Test
+    public void findAllShouldFilterCorrectlyBasedOnMinPrice() {
+        givenThatAllProductsAre(PRODUCT_1, PRODUCT_2, PRODUCT_3);
+
+        List<Product> result = productService.findAllProducts(null, 12, null);
+
+        Assertions.assertThat(result)
+                .containsOnly(PRODUCT_2, PRODUCT_3);
+    }
+
+    @Test
+    public void findAllShouldFilterCorrectlyBasedOnMaxPrice() {
+        givenThatAllProductsAre(PRODUCT_1, PRODUCT_2, PRODUCT_3);
+
+        List<Product> result = productService.findAllProducts(null, null, 12);
+
+        Assertions.assertThat(result)
+                .containsOnly(PRODUCT_1, PRODUCT_2);
+    }
+
+    @Test
+    public void findAllShouldFilterCorrectlyBasedOnMinAndMaxPrice() {
+        givenThatAllProductsAre(PRODUCT_1, PRODUCT_2, PRODUCT_3);
+
+        List<Product> result = productService.findAllProducts(null, 12, 12);
+
+        Assertions.assertThat(result)
+                .containsOnly(PRODUCT_2);
+    }
+
+    @Test
+    public void findAllShouldFilterCorrectlyBasedOnAllFilters() {
+        givenThatAllProductsAre(PRODUCT_1, PRODUCT_2, PRODUCT_3);
+
+        List<Product> result = productService.findAllProducts("book", 12, 12);
+
+        Assertions.assertThat(result)
+                .isEmpty();
+    }
+
+    @Test
     public void updateProductShouldUpdateInTheRepository() {
         Product newProduct = new Product(TEST_ID, "book", 9.5, "old name", "new description");
 
-        givenThatProductForIdIs(TEST_ID, OLD_PRODUCT);
+        givenThatProductForIdIs(TEST_ID, PRODUCT_1);
         givenThatCategoryIsValid(newProduct);
 
         productService.updateProduct(newProduct);
@@ -48,11 +113,11 @@ class ProductServiceTest {
 
         givenThatCategoryIsInvalid(newProduct);
 
-        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        IllegalArgumentException exception = org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, () -> {
             productService.updateProduct(newProduct);
         });
 
-        Assertions.assertTrue(exception.getMessage().contains(newProduct.getCategory()));
+        Assertions.assertThat(exception.getMessage()).contains(newProduct.getCategory());
     }
 
     @Test
@@ -61,11 +126,11 @@ class ProductServiceTest {
 
         givenThatCategoryIsInvalid(newProduct);
 
-        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        IllegalArgumentException exception = org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, () -> {
             productService.createProduct(newProduct);
         });
 
-        Assertions.assertTrue(exception.getMessage().contains(newProduct.getCategory()));
+        Assertions.assertThat(exception.getMessage()).contains(newProduct.getCategory());
     }
 
     private void givenThatProductForIdIs(Long id, Product product) {
@@ -81,5 +146,10 @@ class ProductServiceTest {
     private void givenThatCategoryIsInvalid(Product product) {
         Mockito.when(categoryService.isValidCategory(product.getCategory()))
                 .thenReturn(Boolean.FALSE);
+    }
+
+    private void givenThatAllProductsAre(Product... products) {
+        Mockito.when(productRepository.findAll())
+                .thenReturn(Arrays.asList(products));
     }
 }
