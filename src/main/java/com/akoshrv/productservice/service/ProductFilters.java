ca80bc199f0acba238ac6e3efefb99ff1ad4af58
Akos Horvath <akos.hrv@gmail.com>
2020-05-07 15:05:18 +0200

    Initial Specification implementations

--- src/main/java/com/akoshrv/productservice/service/ProductFilters.java
+++ src/main/java/com/akoshrv/productservice/service/ProductFilters.java
@@ -1,12 +1,25 @@
 package com.akoshrv.productservice.service;
 
 import com.akoshrv.productservice.model.Product;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
 
 import java.util.Objects;
 import java.util.function.Predicate;
 
 public final class ProductFilters {
+
+    public static Specification<Product> hasCategorySpec(String desiredCategory) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("category"), desiredCategory.toLowerCase());
+    }
+
+    public static Specification<Product> priceLargerThanOrEqualsSpec(Integer minPrice) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice);
+    }
+
+    public static Specification<Product> priceSmallerThanOrEqualsSpec(Integer maxPrice) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("price"), maxPrice);
+    }
 
     public static Predicate<Product> hasCategory(String desiredCategory) {
         return product -> {
