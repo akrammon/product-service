637371a8ec4ba79ea45aca2d5058f32ceccebdc9
Akos Horvath <akos.hrv@gmail.com>
2020-05-08 11:18:45 +0200

    Change default constructor visibility

--- src/main/java/com/akoshrv/productservice/model/Product.java
+++ src/main/java/com/akoshrv/productservice/model/Product.java
@@ -17,7 +17,7 @@ public class Product {
     private String name;
     private String description;
 
-    protected Product() {
+    Product() {
     }
 
     public Product(String category, Double price, String name, String description) {
